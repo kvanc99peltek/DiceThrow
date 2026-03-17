463f59df6a993bc56bf835c15249ee3745586930
Kivanc Peltek <kivanc.peltek@temple.edu>
2026-03-17 12:10:01 -0400

    Add second die and wire ROLL button
    Fix die roll range to 1-6, add second DieFragment to layout,
    and connect ROLL button to roll both dice.

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,20 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dieFragment1 = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment
+        val dieFragment2 = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as DieFragment
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+            dieFragment1.throwDie()
+            dieFragment2.throwDie()
+        }
     }
 }
